02cb30ebc5c18247cfd8e106e36646511df59485
youngw0130 <ygw130@naver.com>
2025-06-02 09:03:53 +0900

    feat:10주차

---
+++ src/components/Viewer.jsx
@@ -0,0 +1,23 @@
+import {getEmotionImage} from "../util/get-emotion-image.js";
+import './Viewer.css';
+
+const Viewer = ({content,emotionId}) => {
+    return (
+        <div className="Viewer">
+            <section className="img_section">
+                <h4>오늘의 감정</h4>
+                <div className={`emotion_img_wrapper emotion_img_wrapper_${emotionId}`} >
+                    <img src={getEmotionImage(emotionId)}/>
+                    <div>그럭저럭</div>
+                </div>
+            </section>
+            <section className="content_section">
+                <h4>오늘의 일기</h4>
+                <div className="content_wrapper" >
+                    <p>{content}</p>
+                </div>
+            </section>
+        </div>
+    );
+}
+export default Viewer
